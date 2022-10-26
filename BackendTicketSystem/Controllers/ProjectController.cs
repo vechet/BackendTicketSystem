afb7fe82230ad8e1acdb141590ad5252bcba209a
vechet <vechet240@gmail.com>
2022-10-26 19:09:58 +0700

    implement project api

--- BackendTicketSystem/Controllers/ProjectController.cs
+++ BackendTicketSystem/Controllers/ProjectController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BackendTicketSystem.CustomModels;
+using BackendTicketSystem.Data;
+using BackendTicketSystem.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BackendTicketSystem.Controllers
@@ -7,5 +10,45 @@ namespace BackendTicketSystem.Controllers
     [ApiController]
     public class ProjectController : ControllerBase
     {
+        private readonly BackendTicketSystemContext _db = new BackendTicketSystemContext();
+
+        [HttpGet("Projects")]
+        public IEnumerable<ProjectCustomModel>? Projects(int skip = 0, int limit = 10)
+        {
+            try
+            {
+                var projectList = _db.Projects.Where(x => x.Status.KeyName == "Active");
+
+                var result = projectList.Select(x => new ProjectCustomModel
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    ProjectTypeId=x.ProjectTypeId,
+                    ProjectTypeName=x.ProjectType.Name,
+                    ProjectPackageId = x.ProjectPackageId,
+                    ProjectPackageName = x.ProjectPackage.Name,
+                    WebsiteUrl = x.WebsiteUrl,
+                    ApplicationName = x.ApplicationName,
+                    DatabaseName = x.DatabaseName,
+                    CreatedBy = x.CreatedBy,
+                    CreatedDate = x.CreatedDate,
+                    ModifiedBy = x.ModifiedBy,
+                    ModifiedDate = x.ModifiedDate,
+                    StatusId = x.StatusId,
+                    StatusName = x.Status.Name,
+                    Version=x.Version
+                }).OrderByDescending(x => x.Id).Skip(skip).Take(limit);
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                // ignored
+            }
+            return null;
+        }
+
+        
+
     }
 }
---
+++ BackendTicketSystem/CustomModels/ProjectCustomModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace BackendTicketSystem.CustomModels
+{
+    public class ProjectCustomModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public int ProjectTypeId { get; set; }
+        public string ProjectTypeName { get; set; } = null!;
+        public int ProjectPackageId { get; set; }
+        public string ProjectPackageName { get; set; } = null!;
+        public string WebsiteUrl { get; set; } = null!;
+        public string ApplicationName { get; set; } = null!;
+        public string DatabaseName { get; set; } = null!;
+        public int CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int? ModifiedBy { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+        public short StatusId { get; set; }
+        public string StatusName { get; set; } = null!;
+        public int Version { get; set; }
+    }
+}
+
