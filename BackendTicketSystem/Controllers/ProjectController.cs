148f302066689da6e933ed3c915ed86b56661e28
chuo vechet <vechet240@gmail.com>
2022-10-26 07:20:09 +0700

    analysis database

---
+++ BackendTicketSystem/Controllers/ProjectController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackendTicketSystem.Controllers
+{
+    [Route("api/v1")]
+    [ApiController]
+    public class ProjectController : ControllerBase
+    {
+    }
+}
