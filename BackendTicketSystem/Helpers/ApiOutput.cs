2d9b2fad754d27aaf1b58fb55c25d5252d285609
vechet <vechet240@gmail.com>
2022-11-23 22:17:34 +0700

    update api out model

--- BackendTicketSystem/Helpers/ApiOutput.cs
+++ BackendTicketSystem/Helpers/ApiOutput.cs
@@ -3,8 +3,8 @@
      public class ApiOutput<T>
     {
         public bool Success { get; set; }
-        public string Message { get; set; }
-        public T Data { get; set; }
+        public string Message { get; set; } = null!;
+        public T? Data { get; set; }
         
     }
 }
